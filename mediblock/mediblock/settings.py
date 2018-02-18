07f08a7fc3ae95a3033df6557bc9e19de25390b1
Alex Ahn <alexahn917@gmail.com>
2018-02-18 08:34:14 -0500

    change static path

--- mediblock/mediblock/settings.py
+++ mediblock/mediblock/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = 'https://storage.googleapis.com/mediblock-static/static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/mediblock-static/static/'
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
